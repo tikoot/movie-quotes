bddfa45f354f6de2410328dd1b09e6f732766887
tikoot <tiko.otarashvili99@gmail.com>
2022-08-30 18:05:04 +0200

    refactor:move form to another file like slot

--- resources/views/components/setting.blade.php
+++ resources/views/components/setting.blade.php
@@ -24,24 +24,7 @@
 
         <main class="flex-1">
             <div class="max-w-xl mx-auto">
-                <form method="POST" action="/admin/posts">
-                    @csrf
-          <div class="my-6">
-                <label class="block mb-2  text-lg text-gray-700" for="">Movie Name (English)</label>
-                <input class="text-black text-lg p-2 w-full rounded" type="text" name="title.en">
-            </div>
-            <div class="my-6">
-                <label class="block mb-2  text-lg text-gray-700" for="">Movie Name (Georgian)</label>
-                <input class="text-black text-lg p-2 w-full rounded" type="text" name="title.ka">
-            </div>
-            <div class="my-8">
-                <button type="submit"
-                class="bg-slate-100 text-lg text-black rounded py-2 px-3 hover:bg-dark-gray"
-                >
-                Add Movie
-                </button>
-            </div>
-            </form>
+            {{$slot}}
         </div>
         </main>
     </div>    
--- resources/views/create-movie.blade.php
+++ resources/views/create-movie.blade.php
@@ -1,5 +1,24 @@
 <x-layout>
     <x-header/>
-    <x-setting heading="Add Movie"/>
+    <x-setting heading="Add Movie">
+        <form method="POST" action="/admin/posts">
+            @csrf
+            <div class="my-6">
+                <label class="block mb-2  text-lg text-gray-700" for="">Movie Name (English)</label>
+                <input class="text-black text-lg p-2 w-full rounded" type="text" name="title.en">
+            </div>
+            <div class="my-6">
+                <label class="block mb-2  text-lg text-gray-700" for="">Movie Name (Georgian)</label>
+                <input class="text-black text-lg p-2 w-full rounded" type="text" name="title.ka">
+            </div>
+            <div class="my-8">
+            <button type="submit"
+            class="bg-slate-100 text-lg text-black rounded py-2 px-3 hover:bg-dark-gray"
+            >
+            Add Movie
+            </button>
+        </div>
+    </form>
+    </x-setting>
 </x-layout>
     
