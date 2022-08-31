126b041f72106360b0a10e1882e0bd8b82e0b52d
tikoot <tiko.otarashvili99@gmail.com>
2022-08-31 07:07:07 +0200

    feat:make a model tranlatable

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Movie extends Model
 {
-	use HasFactory;
+	use HasFactory, HasTranslations;
 
 	public $translatable = ['name'];
 }
