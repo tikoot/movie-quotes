2f60a2f3c9669e7dce32be94d7768df54492a4e0
tikoot <tiko.otarashvili99@gmail.com>
2022-08-31 11:02:52 +0200

    feat:create custom form request validation

---
+++ app/Http/Requests/StoreMovieRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreMovieRequest extends FormRequest
+{
+	/*
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function rules()
+	{
+		return [
+			'title_en' => 'required',
+			'title_ka' => 'required',
+		];
+	}
+}
